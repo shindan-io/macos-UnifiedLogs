26e01c2ab316a837522c53f0e2c28e5ae6b0960c
Guillaume M <3082385+mrguiman@users.noreply.github.com>
2024-04-12 11:30:59 +0200

    disable clippy as a workaround

--- benches/high_sierra_benchmark.rs
+++ benches/high_sierra_benchmark.rs
@@ -4,7 +4,7 @@
 // Unless required by applicable law or agreed to in writing, software distributed under the License
 // is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
-
+#![allow(clippy::all, clippy::pedantic, clippy::restriction, clippy::nursery)]
 use std::{fs::File, path::PathBuf};
 
 use criterion::{Criterion, criterion_group, criterion_main};
