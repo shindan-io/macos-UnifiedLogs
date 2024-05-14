d755324ce3bc1803129e4880be0c3cf2b51d2a80
gman <3082385+mrguiman@users.noreply.github.com>
2024-05-14 10:08:51 +0200

    allow clippy in example file

--- examples/unifiedlog_parser/src/main.rs
+++ examples/unifiedlog_parser/src/main.rs
@@ -5,6 +5,7 @@
 // is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 
+#![allow(clippy::all, clippy::pedantic, clippy::restriction, clippy::nursery)]
 use chrono::{SecondsFormat, TimeZone, Utc};
 use log::LevelFilter;
 use macos_unifiedlogs::dsc::SharedCacheStrings;
