1c8cae7756a1736b8282da3420e8e7e2bbc23ebb
jrx <jerome.git@eventuallyconsultant.com>
2026-03-01 22:42:15 +0100

    removed dead code

--- src/decoders/bool.rs
+++ src/decoders/bool.rs
@@ -5,30 +5,9 @@
 // is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 
-/// Return BOOL value to string
-pub(crate) fn uppercase_bool(bool_data: &str) -> &'static str {
-    (bool_data == "0").then(|| "NO").unwrap_or("YES")
-}
-
 const FALSE_BOOL: u32 = 0;
 
 /// Return false if int is 0, true otherwise
 pub(crate) fn bool_from_int(integer: impl Into<u32>) -> bool {
     integer.into() != FALSE_BOOL
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_uppercase_bool() {
-        let mut test_data = "0";
-        let mut results = uppercase_bool(test_data);
-        assert_eq!(results, "NO");
-
-        test_data = "1";
-        results = uppercase_bool(test_data);
-        assert_eq!(results, "YES");
-    }
 }
